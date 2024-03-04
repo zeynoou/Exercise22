18dc8b22fa32a17ae4112217b2bcc11f47c10790
Zeynab <Zeynab@ZEYNAB>
2024-03-04 10:54:23 +0330

    Add project files.

---
+++ Exercise22/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise22
+{
+    class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Please write somthing:");
+        string a = Console.ReadLine();
+        var result = ReverseCase(a);
+        Console.WriteLine(result);
+    }
+
+    public static string ReverseCase(string a)
+    {
+        return string.Concat(a.Select(x => char.IsUpper(x) ? char.ToLower(x) : char.ToUpper(x)));
+    }
+}
+
+}
